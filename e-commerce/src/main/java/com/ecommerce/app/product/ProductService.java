5ecfd6385ce0258cb43afbb79bd93b87745a4cef
OnkarTayde <Onkartayade62@gmail.com>
2024-05-03 02:37:06 +0530

    asdfghjkl

--- e-commerce/src/main/java/com/ecommerce/app/product/ProductService.java
+++ e-commerce/src/main/java/com/ecommerce/app/product/ProductService.java
@@ -31,6 +31,9 @@ public class ProductService
 	{
 		List<ProductModel> productList=new ArrayList<ProductModel>();
 		productRepository.findAll().forEach(productList::add);
+		System.out.println("get all product");
+		System.out.println("get all product");
+		System.out.println("get all product");
 		return productList;
 		
 	}
